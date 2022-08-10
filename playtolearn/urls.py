ed004ff8d36567b543c42fd0a92e95c0fae85c0d
Adam Knapp <aknapp124@gmail.com>
2022-08-10 00:53:48 -0700

    added login form

--- playtolearn/urls.py
+++ playtolearn/urls.py
@@ -17,6 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # Admin
     path('admin/', admin.site.urls),
+
+    # User Management
+    path('account/', include('allauth.urls')),
+
+    # Local Apps
     path('', include('pages.urls')),
 ]

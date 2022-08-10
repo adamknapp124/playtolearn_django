390ed0cf647dd1f1d6dc01af34f30b4d9c2699e6
Adam Knapp <aknapp124@gmail.com>
2022-08-09 22:48:22 -0700

    added postgres

--- playtolearn/settings.py
+++ playtolearn/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'playtolearn.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'playtolearn',
+        'USER': 'postgres',
+        'PASSWORD': 'Talon511!',
+        'HOST': 'localhost',
+        'PORT': 5432
     }
 }
 
